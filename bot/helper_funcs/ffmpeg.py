444f3a05d7b7a1b5cf164a2d56e628a6c06ce4c0
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2024-01-01 12:15:44 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -48,9 +48,18 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
     resolution.append("854x480")
     preset.append("veryfast")
     audio_b.append("40k")
-    watermark.append('-vf "drawtext=fontfile=font.ttf:fontsize=15:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text="AnimeSpectrum')
-    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}" {watermark[0]}  -c:v {codec[0]}  -map 0 -crf {crf[0]} -c:s copy -pix_fmt yuv420p -s {resolution[0]} -b:v 150k -c:a libopus -b:a {audio_b[0]} -preset {preset[0]}  "{out_put_file_name}" -y'
- #Done !!
+    size.append("12")
+    text.append("ninja")
+    metadata.append("ninja")
+    file_generator_command = (
+    f"ffmpeg -hide_banner -loglevel quiet -progress '{progress}' -i '{video_file}' "
+    f"-metadata 'title={metadata[0]}' -c:v {codec[0]} -map 0 -crf {crf[0]} -c:s copy "
+    f"-pix_fmt yuv420p -s {resolution[0]} -b:v 150k -c:a libopus -b:a {audio_b[0]} "
+    f"-preset {preset[0]} -metadata:s:a 'title={metadata[0]}' -metadata:s:s 'title={metadata[0]}' "
+    f'-vf "drawtext=fontfile=font.ttf:fontsize={size[0]}:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text={text[0]}' 
+    f"'{out_put_file_name}' -y"
+  )
+
     COMPRESSION_START_TIME = time.time()
     process = await asyncio.create_subprocess_shell(
           file_genertor_command,
