cee116c9b1649fea3b7c4b3284d2d26bfb207e83
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2024-01-07 08:20:45 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -232,7 +232,7 @@ async def incoming_compress_message_f(update):
          ist = ist_now.strftime("%d/m/%Y, %H:%M:%S")
          bst_now = utc_now + datetime.timedelta(minutes=00, hours=6)
          bst = bst_now.strftime("%d/%m/%Y, %H:%M:%S")
-          now = f"\n{ist} (GMT+05:30)`\n`{bst} (GMT+06:00)"
+         now = f"\n{ist} (GMT+05:30)`\n`{bst} (GMT+06:00)"
           await bot.send_message(chat_id, f"**Upload Stopped, Bot is Free Now !!** \n\nProcess Done at `{now}`", parse_mode="markdown")
           await upload_start.delete()
         except:
